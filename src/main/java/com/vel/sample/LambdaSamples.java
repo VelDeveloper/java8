ea4d28dc4a17737cc0ded603a0f5824b971750d1
Vadivel Murugan <mail2vadivelmurugan@gmail.com>
2017-07-04 23:49:50 +0200

    First commit

---
+++ src/main/java/com/vel/sample/LambdaSamples.java
@@ -0,0 +1,11 @@
+package com.vel.sample;
+
+/**
+ * Created by vadivel on 04/07/17.
+ */
+public class LambdaSamples {
+
+    public static void main(String[] args) {
+        System.out.println("Java8 samples");
+    }
+}
